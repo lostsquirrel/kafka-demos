89bffdfa24e8c506fc9c0be43fd77a807a719b10
lisong <lisong@cdsunrise.net>
2021-07-30 14:37:17 +0800

    add simple comsumer producer demos

---
+++ kafka-client-python/producer/group/producer.py
@@ -0,0 +1,23 @@
+# encoding: utf-8
+import threading
+import time
+from kafka.producer import KafkaProducer
+
+
+class Producer(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.stop_event = threading.Event()
+
+    def stop(self):
+        self.stop_event.set()
+
+    def run(self):
+        producer = KafkaProducer(bootstrap_servers='localhost:9092')
+
+        while not self.stop_event.is_set():
+            producer.send('my-topic', b"test")
+            producer.send('my-topic', b"\xc2Hola, mundo!")
+            time.sleep(1)
+
+        producer.close()
